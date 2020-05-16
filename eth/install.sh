88af63fff47f371bae3ec0a91b7a0cf8dbaeaec6
hay <hay@HAYX021>
2020-05-16 02:40:59 +0200

    Major update

--- eth/install.sh
+++ eth/install.sh
@@ -4,16 +4,18 @@ echo "Download necessary packages"
 sudo apt-get install docker docker-compose git	
 
 echo "Create Data folder"
-sudo mkdir -p /data/ethereum/ntity-01
+sudo mkdir -p /data/blockchain/ntity-01
 
 cd
 git clone https://github.com/ntity-core/nodes
 
-cd /data/ethereum/ntity-01/
-cp /home/docker/nodes/eth/genesis-poa.json .
-cp /home/docker/nodes/eth/static-nodes.json .
+cp nodes/eth/genesis-poa.json /data/ethereum/ntity-01/
+cp nodes/eth/static-nodes.json /data/ethereum/ntity-01/
 
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
 sudo service docker start
-sudo docker run -it -v "/data/ethereum/ntity-01:/ethereum" ntity/sidechain geth --datadir=/ethereum --nousb account new
+
+# Create a new account on Ntity Blockchain
+sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/sidechain geth --datadir=/ethereum --nousb account new
+# FUTUR sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/blockchain geth --datadir=/ethereum --nousb account new
