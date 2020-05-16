103e4f0344332cb0d7eff3141af30cf22921486e
hay <hay@HAYX021>
2020-05-16 03:27:15 +0200

    Major update

--- eth/install.sh
+++ eth/install.sh
@@ -9,13 +9,23 @@ sudo mkdir -p /data/blockchain/ntity-01
 cd
 git clone https://github.com/ntity-core/nodes
 
-cp nodes/eth/genesis-poa.json /data/ethereum/ntity-01/
-cp nodes/eth/static-nodes.json /data/ethereum/ntity-01/
+sudo cp nodes/eth/ntity.genesis.json /data/blockchain/ntity-01/
+sudo cp nodes/eth/static-nodes.json /data/blockchain/ntity-01/
 
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
 sudo service docker start
 
 # Create a new account on Ntity Blockchain
-sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/sidechain geth --datadir=/ethereum --nousb account new
-# FUTUR sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/blockchain geth --datadir=/ethereum --nousb account new
+sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/sidechain geth --datadir=/blockchain --nousb account new
+# FUTUR sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/blockchain geth --datadir=/blockchain --nousb account new
+
+# Log into docker
+sudo docker run -it -v "/data/blockchain/ntity-01:/blockchain" ntity/sidechain /bin/bash
+
+# Init - first time only
+sudo docker run -it -v '/data/blockchain/ntity-01:/blockchain' ntity/sidechain geth --datadir=/blockchain --nousb init /blockchain/ntity.genesis.json
+
+# First Blockchain start
+sudo docker run -it -v '/data/blockchain/ntity-01:/blockchain' ntity/sidechain geth --datadir=/blockchain console
+
