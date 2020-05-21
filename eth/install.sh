e0323c1e7ef17a6a5a0e839bf8bffc7a690bdbf7
hay <hay@HAYX021>
2020-05-21 09:28:20 +0200

    ntity2020 to ntity for genesis

--- eth/install.sh
+++ eth/install.sh
@@ -2,6 +2,7 @@
 
 echo "Download necessary packages"
 sudo apt-get install docker docker-compose git	
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 
 echo "Create Data folder"
 sudo mkdir -p /data/blockchain/ntity-01
@@ -12,6 +13,9 @@ sudo cp nodes/eth/ntity.genesis.json /data/blockchain/ntity-01/
 sudo cp nodes/eth/static-nodes.json /data/blockchain/ntity-01/
 
 sudo groupadd docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
 sudo service docker start
 sudo docker run -it ntity/blockchain /bin/bash
-sudo docker-compose -f ~/nodes/eth/docker-compose-ntity.yml start
+sudo docker-compose -f ~/nodes/eth/docker-compose-ntity.yml up
